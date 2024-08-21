af5437afa67e32b0228b6f5f268d0fd48b6e04d1
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-21 10:38:30 -0300

    Done Exercise 7

--- src/Exercicios7/Carro.java
+++ src/Exercicios7/Carro.java
@@ -7,16 +7,15 @@ public class Carro {
     private float velocidadeMinima;
     private boolean carroLigado;
 
-    public Carro(int numCarro, String nomePiloto, float velocidadeMaxima, float velocidadeMinima) {
+    public Carro(int numCarro, String nomePiloto, float velocidadeMaxima, float velocidadeMinima, boolean carroLigado) {
         this.numCarro = numCarro;
         this.nomePiloto = nomePiloto;
         this.velocidadeMaxima = velocidadeMaxima;
         this.velocidadeMinima = velocidadeMinima;
+        this.carroLigado = carroLigado;
     }
 
-
     /*-----Gets-----*/
-
 
     public int getNumCarro(){
         return numCarro;
@@ -37,24 +36,25 @@ public class Carro {
     /*-----Sets-----*/
 
     public void setNumCarro(int numCarro){
-        this.numCarro = 9;
+        this.numCarro = numCarro;
     }
 
     public void setNomePiloto(String nomePiloto){
-        this.nomePiloto = "Maicon";
+        this.nomePiloto = nomePiloto;
     }
 
     public void setVelocidadeMaxima(float velocidadeMaxima){
-        this.velocidadeMaxima = 200;
+        this.velocidadeMaxima = velocidadeMaxima;
     }
     public void setVelocidadeMinima(float velocidadeMinima){
-        this.velocidadeMinima = 50;
+        this.velocidadeMinima = velocidadeMinima;
     }
 
-
+    public void setCarroLigado(boolean carroLigado){
+        this.carroLigado = carroLigado;
+    }
 
     /*-----Metodos-----*/
-
 
     public void carro(){
         System.out.println(getNumCarro());
@@ -62,26 +62,39 @@ public class Carro {
     }
 
     public void acelerar(){
-        if(getCarroLigado() == true && getVelocidadeMinima() == 0){
-
+        if(carroLigado == true && velocidadeMinima == 0){
+            setVelocidadeMinima(10);
+            System.out.println("O carro acelerou para a velocidade " + getVelocidadeMinima());
         }
     }
 
     public void parar(){
-
+        if(velocidadeMinima != 0 && velocidadeMaxima != 0 && carroLigado == true){
+            setVelocidadeMaxima(0);
+            System.out.println("O carro esta parado e a velocidade eh de " + getVelocidadeMaxima());
+        }
     }
 
     public void frear(){
-
+        if(carroLigado == true && velocidadeMinima != 0 && velocidadeMaxima > 0){
+             float var = (getVelocidadeMaxima() * 30) / 100;
+             setVelocidadeMaxima(var);
+             System.out.println("O carro freou e a velocidade atual eh de " + getVelocidadeMaxima());
+        }
     }
-    public void desligar(boolean carroLigado){
-        if (!carroLigado){
-            System.out.println("Carro Desligado");
+
+    public void desligar(){
+        if (carroLigado == true && velocidadeMinima == 0 && velocidadeMaxima == 0){
+            setCarroLigado(false);
+            System.out.println("O carro esta desligado " + getCarroLigado());
         }
     }
 
     public void ligar(){
-
+        if (carroLigado == false && velocidadeMinima == 0 && velocidadeMaxima == 0){
+            setCarroLigado(true);
+            System.out.println("O carro esta ligado " + getCarroLigado());
+        }
     }
 
 }
--- src/Exercicios7/Main.java
+++ src/Exercicios7/Main.java
@@ -2,9 +2,14 @@ package Exercicios7;
 
 public class Main {
     public static void main(String[] args) {
-        Carro carro1 = new Carro(2,"Guilherme", 100, 0);
+        Carro carro1 = new Carro(2,"Guilherme", 0, 0, false );
 
         carro1.carro();
+        // carro1.parar();
+        // carro1.frear();
+        // carro1.desligar();
+        carro1.ligar();
+        carro1.acelerar();
 
     }
 }
