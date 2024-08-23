8a3a576b8d684ebb1c7d2fe9c169cf0fbce37c29
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-23 19:22:33 -0300

    Done Exercise 09

--- src/Exercicios9/Main.java
+++ src/Exercicios9/Main.java
@@ -3,9 +3,14 @@ package Exercicios9;
 public class Main {
     public static void main(String[] args) {
         Funcionario  funcionario1 = new Funcionario("joao", 44, 1500);
+        Funcionario  funcionario2 = new Funcionario("pablo", 50, 2500);
 
         funcionario1.hrsTrabalhadasMes();
         funcionario1.salarioHrs();
         funcionario1.salarioTotal();
+
+        funcionario2.hrsTrabalhadasMes();
+        funcionario2.salarioHrs();
+        funcionario2.salarioTotal();
     }
 }
